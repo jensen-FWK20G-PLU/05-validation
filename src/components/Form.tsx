8f594d2cc0ba5694d5e502fdef45f3c6ef49816a
David Andersson <david.andersson@zocom.se>
2022-02-09 11:04:20 +0100

    Validation of name field

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,38 @@
+import { useState } from 'react'
 import './Form.css'
 
+
 const Form = () => {
+	const [name, setName] = useState<string>('')
+	const [nameIsVisited, setNameIsVisited] = useState<boolean>(false)
+	
+	const [nameIsValid, nameMessage] = isValidName(name);
+
+	const nameInputCss = !nameIsVisited ? '' : (nameIsValid ? 'valid' : 'invalid');
+	const nameMessageCss = (nameIsVisited ? '' : 'invisible') + (nameIsValid ? '' : ' error')
+
 
 	return (
 		<div className="form">
 			<h2> Användarprofil </h2>
 			<section>
 				<label>Namn</label>
-				<input className="" type="text" placeholder="Ada Lovelace" />
-				<span className="error invisible"> Felmeddelande </span>
+				<input
+					className={nameInputCss}
+					type="text"
+					placeholder="Ada Lovelace"
+					value={name}
+					onChange={e => setName(e.target.value)}
+					onBlur={() => setNameIsVisited(true)} />
+				<span className={nameMessageCss}> {nameMessage} </span>
 			</section>
+
 			<section>
 				<label>Profilbild</label>
 				<input className="valid" type="text" placeholder="länk/bild.jpg" />
 				<span className=""> ✔️ </span>
 			</section>
+
 			<section>
 				<label>Födelseår</label>
 				<input className="error invalid" type="text" placeholder="Årtal med fyra siffror" />
@@ -28,5 +46,15 @@ const Form = () => {
 		</div>
 	)
 }
+
+
+function isValidName(name: string): [boolean, string] {
+	if( name.length >= 2 ) {
+		return [true, '✔️']
+	} else {
+		return [false, '❌ Skriv ditt namn, minst två bokstäver.']
+	}
+}
+
 
 export default Form;
